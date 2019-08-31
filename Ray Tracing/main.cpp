37689be741dff0f3b58f8ac7ceda237c5cf8862b
DharshanV <38626675+DharshanV@users.noreply.github.com>
2019-08-30 22:24:07 -0700

    Added progress images

--- Ray Tracing/main.cpp
+++ Ray Tracing/main.cpp
@@ -4,9 +4,11 @@ int main() {
 	uint32_t width = 500;
 	uint32_t height = 500;
 	const char* fileName = "Rendered-Image.ppm";
-	Renderer renderer(width, height);
+	Renderer renderer(width, height,Vector3f(0,0,3));
 
-	renderer.addObject(new Sphere(Vector3f(0,0,-3), 1));
+	renderer.addObject(new Sphere(Vector3f(-2, 0, -3), 1));
+	renderer.addObject(new Sphere(Vector3f(2, 0, -5), 1));
+	renderer.addObject(new Sphere(Vector3f(0, -3, -10), 1));
 
 	renderer.start();
 	renderer.output(fileName);
