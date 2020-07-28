63d3aa5e3c3477cda933d1e9cb93c9508ffc4b98
DharshanV <ddvyukjng7@gmail.com>
2020-07-27 23:53:01 -0700

    Added more test for Vector3

--- test/test_Vector3.cpp
+++ test/test_Vector3.cpp
@@ -2,20 +2,19 @@
 #include <string>
 
 #include <gtest/gtest.h>
+#include <sstream>
 #include "Vector3.h"
 
 using namespace testing;
 
 class Vector3Tester : public ::testing::Test {
   protected:
-  Vector3<int> vec3;
   public:
-    Vector3Tester() {
-      vec3 = Vector3<int>(1,2,3);
-    }
+    Vector3Tester() { }
 };
 
 TEST_F(Vector3Tester, TEST_INDEXING) {
+  Vector3<int> vec3 = Vector3<int>(1,2,3);
   ASSERT_EQ(vec3[0],1);
   ASSERT_EQ(vec3[1],2);
   ASSERT_EQ(vec3[2],3);
@@ -27,4 +26,30 @@ TEST_F(Vector3Tester, TEST_INDEXING) {
   ASSERT_EQ(vec3[0],4);
   ASSERT_EQ(vec3[1],5);
   ASSERT_EQ(vec3[2],6);
+}
+
+TEST_F(Vector3Tester, TEST_PRINT) {
+  Vector3<int> vec3 = Vector3<int>(1,2,3);
+  std::stringstream stream;
+  vec3.print(stream);
+  ASSERT_EQ(stream.str(),"[1,2,3]");
+}
+
+TEST_F(Vector3Tester, TEST_NORMALIZE) {
+  Vector3<float> vec3 = Vector3<float>(-1,2,3);
+  float scale = vec3.normalize();
+  ASSERT_FLOAT_EQ(scale,sqrtf32(1+4+9));
+}
+
+TEST_F(Vector3Tester, TEST_GET_NORMALIZED) {
+  Vector3<float> vec3 = Vector3<float>(-1,2,3);
+  Vector3<float> nVec3 = vec3.getNormalized();
+
+  float scale = vec3.normalize();
+  float trueValues[] = {-1.0f/scale,2.0f/scale,3.0f/scale};
+  for(int i=0;i<3;i++){
+    ASSERT_FLOAT_EQ(nVec3[i],trueValues[i]);
+  }
+
+  ASSERT_FLOAT_EQ(nVec3.length(),1);
 }
